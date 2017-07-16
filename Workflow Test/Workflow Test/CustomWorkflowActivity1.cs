a1ae6f73d0bd9bc33e657b5f60a5c6156cab25f1
Friyank Parikh <pppriyankparikh@gmail.com>
2017-07-16 23:48:54 +0530

    new project

--- Workflow Test/Workflow Test/CustomWorkflowActivity1.cs
+++ Workflow Test/Workflow Test/CustomWorkflowActivity1.cs
@@ -11,6 +11,18 @@ namespace Workflow_Test
 {
     public class CustomWorkflowActivity1 : CodeActivity
     {
+        #region Input Properties
+        [Input("string input")]
+        public InArgument<string> ContactFirstName { get; set; }
+
+        [Input("string input")]
+        public InArgument<string> ContactLastName { get; set; }
+        #endregion
+
+        #region Output properties
+        public OutArgument<Guid> CreatedContactId{ get; set; }
+        #endregion
+
         protected override void Execute(CodeActivityContext context)
         {
             #region Tracing Object
@@ -34,6 +46,11 @@ namespace Workflow_Test
             {
                 traceObj.Trace("Workflow Starts successfully");
 
+                Entity CreateContactEntity = new Entity("contact");
+                CreateContactEntity["firstname"] = ContactFirstName.Get(context);
+                CreateContactEntity["lastname"] = ContactLastName.Get(context);
+
+                CreatedContactId.Set(context, orgServiceConext.Create(CreateContactEntity);
 
                 traceObj.Trace("Workflow End successfully");
             }
